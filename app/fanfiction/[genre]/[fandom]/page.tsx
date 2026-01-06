0143984fecc62c16e562d3ef8fa4037779bb787f
Dani MacInnes <dmacinnes@transperfect.com>
2026-01-06 15:08:05 -0500

    finishing up fandom page

--- app/fanfiction/[genre]/[fandom]/page.tsx
+++ app/fanfiction/[genre]/[fandom]/page.tsx
@@ -1,13 +1,42 @@
-const FandomPage = () => {
+import { Box, Title } from "@mantine/core";
+import { sanityFetch } from "../../../../sanity/lib/live";
+import { STORIES_FOR_FANDOM_QUERY } from "../../../../sanity/lib/queries";
+import { STORIES_FOR_FANDOM_QUERYResult } from "../../../../sanity/types";
+import NotFound from "../../../not-found";
+import HeroSection from "../../../_components/HeroSection";
+import StoryGrid from "../../../_components/StoryGrid";
+
+const FandomPage = async ({ params }: { params: { fandom: string } }) => {
+  const { fandom } = await params;
+
+  const { data: stories } = await sanityFetch({
+    query: STORIES_FOR_FANDOM_QUERY,
+    params: { fandomSlug: fandom },
+  });
+
+  const typedStories = stories as STORIES_FOR_FANDOM_QUERYResult;
+
+  const fandomName = typedStories[0]?.fandom.title;
+
+  const numCols = typedStories.length < 5 ? typedStories.length : 5;
+
+  if (!typedStories || typedStories.length === 0) {
+    return <NotFound />;
+  }
+
   return (
-    <div>
-      <h1>Fandom</h1>
-      {/* 
-        Show cards with all fanfics for this fandom.
-          1. Fetch all stories for this fandom.
-          2. For each story, fetch the first chapter.
-      */}
-    </div>
+    <Box>
+      <HeroSection
+        title={fandomName}
+        subtitle={`Browse all of my ${fandomName} fanfiction!`}
+      />
+      <Box mx="auto" my="4rem">
+        <Title order={2} ta="center" my="2rem">
+          Stories
+        </Title>
+        <StoryGrid stories={typedStories} cols={numCols} />
+      </Box>
+    </Box>
   );
 };
 
