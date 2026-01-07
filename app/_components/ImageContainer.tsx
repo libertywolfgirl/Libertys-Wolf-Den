6592fab3e1f87da83ef84143a35d89075d61c5cc
Dani MacInnes <dmacinnes@transperfect.com>
2026-01-07 12:24:20 -0500

    creating ImageContainer

---
+++ app/_components/ImageContainer.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { urlFor } from "../_utils/imageUrl";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { Flex } from "@mantine/core";
+
+const ImageContainer = ({
+  image,
+  title,
+  maximumHeight,
+  padding,
+}: {
+  image: SanityImageSource;
+  title: string;
+  maximumHeight?: number;
+  padding?: string;
+}) => {
+  return (
+    <Flex justify="center" align="center" p={padding || 0}>
+      <Image
+        src={urlFor(image).url()}
+        alt={title}
+        width={400}
+        height={800}
+        style={{
+          width: "auto",
+          height: "100%",
+          maxHeight: maximumHeight || 800,
+        }}
+      />
+    </Flex>
+  );
+};
+
+export default ImageContainer;
--- app/_components/StoryCard.tsx
+++ app/_components/StoryCard.tsx
@@ -1,8 +1,7 @@
-import { Button, Card, Flex, Group, Stack, Text, Title } from "@mantine/core";
-import { Story, STORY_QUERYResult } from "../../sanity/types";
-import Image from "next/image";
-import { urlFor } from "../_utils/imageUrl";
+import { Button, Card, Group, Stack, Text, Title } from "@mantine/core";
+import { STORY_QUERYResult } from "../../sanity/types";
 import Link from "next/link";
+import ImageContainer from "./ImageContainer";
 
 type StoryCardStory = NonNullable<STORY_QUERYResult>;
 
@@ -12,17 +11,8 @@ const StoryCard = ({ story }: { story: StoryCardStory }) => {
   return (
     <Card mih={500} maw={300} shadow="sm" radius="md" withBorder>
       {image && (
-        <Flex align="center" justify="center">
-          <Image
-            src={urlFor(image).url()}
-            alt={title}
-            width={300}
-            height={600}
-            style={{ width: "auto", height: "100%", maxHeight: 200 }}
-          />
-        </Flex>
+        <ImageContainer image={image} title={title} maximumHeight={200} />
       )}
-
       <Stack h="100%" justify="space-evenly" gap="0.5rem">
         <Group justify="space-between" mt="xs">
           <Link
@@ -43,7 +33,6 @@ const StoryCard = ({ story }: { story: StoryCardStory }) => {
         <Title order={6}>{title}</Title>
         {summary && <Text lineClamp={6}>{summary}</Text>}
       </Stack>
-
       <Link
         href={`/fanfiction/${genre.slug.current}/${fandom.slug.current}/${slug.current}`}
         style={{ textDecoration: "none" }}
