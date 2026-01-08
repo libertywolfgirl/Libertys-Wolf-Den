49ad2eec14dd8e81d77c116b7a28269a4846ea3d
Dani MacInnes <dmacinnes@transperfect.com>
2026-01-08 11:45:15 -0500

    creating StoryInfo

--- app/_components/StoryInfo.tsx
+++ app/_components/StoryInfo.tsx
@@ -1,4 +1,6 @@
-import { Group, Stack, Text, Title } from "@mantine/core";
+"use client";
+
+import { Table, Text, Title } from "@mantine/core";
 import { STORY_WITH_FIRST_CHAPTER_QUERYResult } from "../../sanity/types";
 
 const StoryInfo = ({
@@ -9,26 +11,51 @@ const StoryInfo = ({
   const { title, summary, pairings, notes } = story || {};
 
   return (
-    <Stack
-      maw={1400}
-      mx="auto"
-      p="2rem"
-      style={{
-        border: "1px solid var(--mantine-color-green-9)",
-        borderRadius: "8px",
-      }}
-    >
-      <Group>
-        <Title order={6}>Title:</Title>
-        <Text>{title}</Text>
-      </Group>
-      {summary && (
-        <Group>
-          <Title order={6}>Summary: </Title>
-          <Text maw={1200}>{summary}</Text>
-        </Group>
-      )}
-    </Stack>
+    <Table variant="vertical" withTableBorder maw={1400} mx="auto" p="2rem">
+      <Table.Tbody>
+        <Table.Tr>
+          <Table.Th p="1rem">
+            <Title order={6}>Title</Title>
+          </Table.Th>
+          <Table.Td p="1rem">
+            <Text>{title}</Text>
+          </Table.Td>
+        </Table.Tr>
+
+        {summary && (
+          <Table.Tr>
+            <Table.Th p="1rem">
+              <Title order={6}>Summary</Title>
+            </Table.Th>
+            <Table.Td p="1rem">
+              <Text>{summary}</Text>
+            </Table.Td>
+          </Table.Tr>
+        )}
+
+        {pairings && (
+          <Table.Tr>
+            <Table.Th p="1rem">
+              <Title order={6}>Pairings</Title>
+            </Table.Th>
+            <Table.Td p="1rem">
+              <Text>{pairings}</Text>
+            </Table.Td>
+          </Table.Tr>
+        )}
+
+        {notes && (
+          <Table.Tr>
+            <Table.Th p="1rem">
+              <Title order={6}>Notes</Title>
+            </Table.Th>
+            <Table.Td p="1rem">
+              <Text>{notes}</Text>
+            </Table.Td>
+          </Table.Tr>
+        )}
+      </Table.Tbody>
+    </Table>
   );
 };
 
