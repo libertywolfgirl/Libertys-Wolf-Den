c753a1d20da576cab2ac01e3b1ceeae065408f3a
Dani MacInnes <dmacinnes@transperfect.com>
2026-03-17 16:10:19 -0400

    adding completed stories to search

--- app/_components/SearchBar.tsx
+++ app/_components/SearchBar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { TextInput } from "@mantine/core";
+import { Checkbox, Group, TextInput, Text, Stack } from "@mantine/core";
 import { IconSearch } from "@tabler/icons-react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import useDebounce from "../_hooks/useDebounce";
 
 const SearchBar = () => {
   const router = useRouter();
@@ -12,36 +13,49 @@ const SearchBar = () => {
   const searchParams = useSearchParams();
 
   const [inputValue, setValue] = useState("");
+  const [checked, setChecked] = useState(false);
+  const debouncedInputValue = useDebounce(inputValue, 1000);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const inputValue = event.target.value;
     setValue(inputValue);
   };
 
-  const handleSearch = () => {
+  useEffect(() => {
     const params = new URLSearchParams(searchParams);
-
-    if (inputValue) {
-      params.set("query", inputValue);
+    if (debouncedInputValue) {
+      params.set("query", debouncedInputValue);
     } else {
       params.delete("query");
     }
+    if (checked) {
+      params.set("completed", "true");
+    } else {
+      params.delete("completed");
+    }
     replace(`${pathname}?${params.toString()}`);
     router.refresh();
-  };
-
-  const handleKeyPress = (event: { key: any }) => {
-    if (event.key === "Enter") return handleSearch();
-  };
+  }, [debouncedInputValue, checked]);
 
   return (
-    <TextInput
-      placeholder="Search fanfiction..."
-      value={inputValue ?? ""}
-      onChange={handleChange}
-      onKeyDown={handleKeyPress}
-      rightSection={<IconSearch size={20} />}
-    />
+    <Stack align="center" w="100%">
+      <Text fw={600} fz="1.5rem" pb="md">
+        Looking for something specific?
+      </Text>
+      <Group justify="center" gap="xl" w="100%">
+        <TextInput
+          placeholder="Search fanfiction..."
+          value={inputValue ?? ""}
+          onChange={handleChange}
+          rightSection={<IconSearch size={20} />}
+          w={{ base: "100%", md: "75%" }}
+        />
+        <Checkbox
+          label="Completed?"
+          onChange={(event) => setChecked(event.currentTarget.checked)}
+        />
+      </Group>
+    </Stack>
   );
 };
 
